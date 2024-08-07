b98e7c1ab2e23d46053a13a0c36ea7d697617edb
Nils-Markus Meister <meister.markus.MM@gmx.net>
2024-08-07 20:14:32 +0200

    Update print_2d_curves.c

--- src/print_2d_curves.c
+++ src/print_2d_curves.c
@@ -6,8 +6,9 @@
 
 int main() {
     int d = 9;
-    int verbose = 1;
+    int verbose = 0;
     int sub = 0;
+    char op='n';
 
     printf("Welcome to the 2D space filling curves!\n");
     printf("This is a demo outputting a 2D map of the indices.\n");
@@ -15,6 +16,11 @@ int main() {
     printf("And keep in mind that large matrices couldn't be displayed here!\n");
     printf("Please input you desired side length: ");
     scanf(" %d", &d);
+    printf("Do you want to run the verb ose mode?\n(y/n)\n");
+    scanf(" %c", &op);
+    if (op == 'y'){
+        verbose = 1;
+    }
 
     // final recursive mapper function
     gsl_matrix *map = hilbert_peano(d, verbose, sub);
